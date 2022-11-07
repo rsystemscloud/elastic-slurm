b6fd1f348ca80754d2aa0eb2003551e3c7953401
vdudejon <jon.somers@gmail.com>
2022-11-07 10:55:45 -0600

    Update and rename addnodes.ps1 to resume.ps1

--- resume.ps1
+++ resume.ps1
@@ -1,20 +1,38 @@
 #!/usr/bin/pwsh
-param ($list)
-$count = ($list | convertfrom-json).count
+param ($nodelist)
 
 # Set vars
+$token = ""
 $account = ""
 $projid = ""
 $blueprintid = ""
-$token = ""
 $vraserver = "api.mgmt.cloud.vmware.com"
+
+# Install PowerVRA if needed
+Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
+install-module PowerVRA
+
+$nodes = /usr/bin/scontrol show hostnames $nodelist
+$count = $nodes.count
+$list = $nodes | convertto-json
+if ($count -eq 1) {
+    $list = @"
+    ["$nodes"]
+"@
+}
+
+write-host "Debug vars:" $list
+write-host "Debug count:" $count
 
 # Connect to vRA
 $res = Connect-vRAServer -Server $vraserver -APItoken $token
+#Write-host "Debug Connect-vRA Server" $res
 
 
 # This is unix timestamp in seconds, to use in the name of the deployment
 $timestamp = Get-Date -UFormat %s
+
+# Account here can be hard coded, since it's always AWS Truepower
 $name = "SLURM Created $timestamp-$account"
 
 # Build the request
@@ -28,5 +46,6 @@ $body = @"
   "projectId": "$projid"
 }
 "@
+write-host "Debug json:" $body
 $res = Invoke-vRARestMethod -Method Post -URI "/catalog/api/items/$blueprintid/request" -Body $body
 return $res
