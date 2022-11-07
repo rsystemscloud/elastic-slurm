419adad9d457b4f9a3e079c9ca2ddc60314fd766
vdudejon <jon.somers@gmail.com>
2022-11-07 10:56:52 -0600

    Update and rename deletenodes.ps1 to suspend.ps1

--- suspend.ps1
+++ suspend.ps1
@@ -1,19 +1,25 @@
 #!/usr/bin/pwsh
 param ($list)
-$data = $list | convertfrom-json
-$count = $data.count
 
 # Set vars
+$token = ""
 $account = ""
 $projid = ""
-$token = ""
 $vraserver = "api.mgmt.cloud.vmware.com"
+
+# Install PowerVRA if needed
+Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
+install-module PowerVRA
+
+$data = $list | convertfrom-json
+$count = $data.count
 
 # Connect to vRA
 $res = Connect-vRAServer -Server $vraserver -APItoken $token
 
 # Get potential deployments
 $deployments = Get-vRADeployment | Where-Object {($_.ProjectId -eq $projid) -and ($_.Name -like "SLURM*")}
+Write-host "Debug deployments:" $deployments
 
 # Find the VMs
 $list = @()
@@ -29,7 +35,7 @@ foreach ($deployment in $deployments){
 
     }
 }
-
+write-host "Debug list:" $list
 # Exit if it can't find the VM ids
 if (!$list){
     $errormsg =  "errormsg: Could not find the VMs"
@@ -37,19 +43,16 @@ if (!$list){
     exit 1
 }
 
-# Get unique deployment ids
 $deploymentids = $list.deploymentid | Select-Object -Unique
+#write-host "Debug deploymentids:" $deploymentids
 
-# Loop through deployments tofind the VMs, then delete them
 foreach ($deploymentid in $deploymentids){
     # Get all VMs in the deployment
     $vms = (Invoke-vRARestMethod -Uri /deployment/api/deployments/$deploymentid/resources -Method Get).content
     $vms = $vms | Where-Object type -eq "Cloud.vSphere.Machine"
     $vmcount = ($list | Where-Object deploymentid -eq $deploymentid).count
-
     # If deleting all VMs in the deployment, just delete the deployment
     if ($vms.count -eq $vmcount) {
-#       write-host "Debug: Deleting deployment"
         $res = Invoke-vRARestMethod -Uri /deployment/api/deployments/$deploymentid -Method Delete
         write-host $res
     }
